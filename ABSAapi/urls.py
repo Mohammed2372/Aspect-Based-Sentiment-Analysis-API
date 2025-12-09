c2e0f33346326341a5956e0641d4fa26959d3603
Mohammed237 <mohammedmostafa2372002@gmail.com>
2025-12-10 01:20:42 +0200

    make user login from page with api-auth

--- ABSAapi/urls.py
+++ ABSAapi/urls.py
@@ -22,6 +22,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("analyzer.urls")),
+    path("api-auth/", include("rest_framework.urls")),
     # --- Swagger Routes ---
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
